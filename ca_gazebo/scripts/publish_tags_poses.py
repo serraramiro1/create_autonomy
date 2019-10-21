24dcd07a31a338d2bea7f97b9af7540258253588
Ramiro Serra <serraramiro@creativa77.com>
2019-10-21 16:41:53 -0300

    Adressed requested changes
    renamed grey square, added warn message to the pose publisher node, reworked launchfile, added tf dependency

--- ca_gazebo/scripts/publish_tags_poses.py
+++ ca_gazebo/scripts/publish_tags_poses.py
@@ -1,35 +1,40 @@
 #!/usr/bin/env python  
 import roslib
 import rospy
-import math
 import tf
 import geometry_msgs.msg
-import turtlesim.srv
 
 #Publishes the tag 1 pose relative to the /map frame, to "tags_gts/pose" topic
 if __name__ == '__main__':
-    rospy.init_node('tags_tf_to_pose')
+    rospy.init_node('tags_tf_2_map_pose')
 
     listener = tf.TransformListener()
 
-    tag_pose = rospy.Publisher('tags_gts/pose', geometry_msgs.msg.PoseStamped,queue_size=1)
-
-    rate = rospy.Rate(10.0)
+    tag_pose_pub = rospy.Publisher('tags_gts/pose', geometry_msgs.msg.PoseStamped, queue_size=1)
+    param_name = rospy.search_param('tags_tf_2_map_rate')
+    rate_param = rospy.get_param(param_name)
+    rate = rospy.Rate(rate_param)
     pose = geometry_msgs.msg.PoseStamped()
+    pose.header.frame_id="/map"
+    seq=0
     while not rospy.is_shutdown():
         try:
             (trans,rot) = listener.lookupTransform('/map', '/tag_1', rospy.Time(0))
             pose.pose.position.x=trans[0]
             pose.pose.position.y=trans[1]
             pose.pose.position.z=trans[2]
-            pose.header.frame_id="/map"
+
+            pose.header.seq=seq
+            pose.header.stamp=rospy.Time.now()
             pose.pose.orientation.x=rot[0]
             pose.pose.orientation.y=rot[1]
             pose.pose.orientation.z=rot[2]
             pose.pose.orientation.w=rot[3]
 
-            tag_pose.publish(pose)
+            tag_pose_pub.publish(pose)
+            seq+=1
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            rospy.logwarn_throttle(2 , "Error getting transform from /map to /tag_1")
             continue
 
         rate.sleep()
